c62f8a68f5a4485a9bb3012927874fa1da5da1f1
faisal-shohag <faisalshohagprog@gmail.com>
2024-12-11 20:30:51 +0600

    cookie config changes

--- index.js
+++ index.js
@@ -15,15 +15,14 @@ const PORT = process.env.PORT || 5000;
 //   secure: process.env.NODE_ENV === "production",
 //   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
 // };
-//["http://localhost:5173", "https://sora-vocab.vercel.app", "https://sora-vocab.netlify.app"]
 
-app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://sora-vocab.vercel.app",
+    origin: ["http://localhost:5173", "https://sora-vocab.vercel.app", "https://sora-vocab.netlify.app"],
     credentials: true,
   })
 );
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/auth', authRouter)
