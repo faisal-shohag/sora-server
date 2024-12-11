a59d407ae99810925e70dc2d6a1aa28c6642563c
faisal-shohag <faisalshohagprog@gmail.com>
2024-12-11 19:04:21 +0600

    resolving cors issue

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://sora-vocab.netlify.app"],
+    origin: ["http://localhost:5173", "https://sora-vocab.vercel.app"],
     credentials: true,
   })
 );
