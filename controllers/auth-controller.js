43032fafd32a0b795c76088ed2caf8fd90eeee69
faisal-shohag <faisalshohagprog@gmail.com>
2024-12-11 19:14:23 +0600

    cookie config changes

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -21,7 +21,7 @@ const signup = async (req, res) => {
     res.cookie("token", jwtToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      // sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     }).json({message: "Successfully signedup!"});
   } catch (err) {
@@ -51,7 +51,7 @@ const login = async (req, res) => {
     res.cookie("token", jwtToken, {
         httpOnly: true,
         secure: true,
-        sameSite: "none",
+        // sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       }).json({message: "Successfully logged in!"});
 
